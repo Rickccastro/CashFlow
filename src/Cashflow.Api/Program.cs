538d9bbbb7e1a422ebde96e34d4e9fde64ae2ade
ricardo <ricardoparmezancastro.dev@gmail.com>
2024-09-24 14:14:36 -0300

    Autorização nos endpoints

--- src/Cashflow.Api/Program.cs
+++ src/Cashflow.Api/Program.cs
@@ -3,7 +3,11 @@ using Cashflow.Api.Middleware;
 using CashFlow.Application;
 using CashFlow.Infraestructure;
 using CashFlow.Infraestructure.Migrations;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using System.Text;
 
 namespace Cashflow.Api;
 
@@ -15,16 +19,64 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        builder.Services.AddSwaggerGen(config =>
+        {
+            config.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Description = @"JWT Authorization header using the Bearer scheme.
+                                Enter 'Bearer' [space] and then your token is the next text input below.
+                                Example:'Bearer 1234abcdef'",
+                In = ParameterLocation.Header,
+                Scheme = "Bearer",
+                Type = SecuritySchemeType.ApiKey
+            });
+
+            config.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header
+                        },
+                        new List<string>()
+                    }
+                });
+        });
 
 
-        builder.Services.AddMvc(options =>options.Filters.Add(typeof(ExceptionFilter)));
-       
+        builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
+
         builder.Services.AddInfrastructure(builder.Configuration);
-
         builder.Services.AddApplication();
 
-        builder.Services.AddSwaggerGen(options => {
+        var signingKey = builder.Configuration.GetValue<string>("Settings:Jwt:SigningKey");
+
+
+        builder.Services.AddAuthentication(config =>
+        {
+            config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(config =>
+        {
+            config.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ClockSkew = new TimeSpan(0),
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(signingKey!))
+            };
+        });
+
+        builder.Services.AddSwaggerGen(options =>
+        {
             options.MapType<DateOnly>(() => new OpenApiSchema
             {
                 Type = "string",
@@ -44,20 +96,22 @@ public class Program
 
         app.UseHttpsRedirection();
 
+        app.UseAuthentication();
+
         app.UseAuthorization();
 
         app.MapControllers();
 
         app.Run();
-        await MigrateDatabase(); 
+        await MigrateDatabase();
 
         app.Run();
 
         async Task MigrateDatabase()
         {
-           await using var scope = app.Services.CreateAsyncScope();
-           
-           await DataBaseMigration.MigrateDatabase(scope.ServiceProvider);
+            await using var scope = app.Services.CreateAsyncScope();
+
+            await DataBaseMigration.MigrateDatabase(scope.ServiceProvider);
 
         }
     }
